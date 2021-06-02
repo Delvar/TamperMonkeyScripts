a17c2f06b2a39ef173cb6015003d06522d247f14
Morgan Gilroy <morgan.gilroy@gmail.com>
2021-06-02 17:06:00 +0100

    Update SalesforceConsoleResizer.user.js

--- SalesforceConsoleResizer.user.js
+++ SalesforceConsoleResizer.user.js
@@ -13,7 +13,7 @@
     // https://github.com/Delvar/TamperMonkeyScripts/raw/main/SalesforceConsoleResizer.user.js
 
     'use strict';
-    GM_addStyle('[flexipage-recordhomepinnedheaderleftsidebartwocoltemplatedesktop2_recordhomepinnedheaderleftsidebartwocoltemplatedesktop2].slds-region__pinned-left {overflow-y: auto; position: relative; resize: horizontal; min-width: 400px; max-width: 800px; width: 400px;}');
+    GM_addStyle('[flexipage-recordhomepinnedheaderleftsidebartwocoltemplatedesktop2_recordhomepinnedheaderleftsidebartwocoltemplatedesktop2].slds-region__pinned-left {overflow-y: auto; position: relative; resize: horizontal; min-width: 25%; max-width: 50%; width: 25%;}');
     GM_addStyle('[flexipage-recordhomepinnedheaderleftsidebartwocoltemplatedesktop2_recordhomepinnedheaderleftsidebartwocoltemplatedesktop2].main-container-wrapper {flex: auto}');
     GM_addStyle('div.tabContainer.oneConsoleTabContainer.slds-grid { flex: auto; width: 100% !important;}');
 })();
