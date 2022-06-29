dbb26df976178fe3160cd167661ead96fc6aa7d2
Morgan Gilroy <morgan.gilroy@gmail.com>
2022-06-29 12:52:02 +0100

    Update SalesforceConsoleResizer.user.js
    Fixed for new 2  and 3 pain layouts

--- SalesforceConsoleResizer.user.js
+++ SalesforceConsoleResizer.user.js
@@ -11,9 +11,11 @@
 
 (function() {
     // https://github.com/Delvar/TamperMonkeyScripts/raw/main/SalesforceConsoleResizer.user.js
-
     'use strict';
-    GM_addStyle('[flexipage-recordhomepinnedheaderleftsidebartwocoltemplatedesktop2_recordhomepinnedheaderleftsidebartwocoltemplatedesktop2].slds-region__pinned-left {overflow-y: auto; position: relative; resize: horizontal; min-width: 25%; max-width: 50%; width: 25%;}');
-    GM_addStyle('[flexipage-recordhomepinnedheaderleftsidebartwocoltemplatedesktop2_recordhomepinnedheaderleftsidebartwocoltemplatedesktop2].main-container-wrapper {flex: auto}');
-    GM_addStyle('div.tabContainer.oneConsoleTabContainer.slds-grid { flex: auto; width: 100% !important;}');
+    GM_addStyle('.main-container-wrapper { flex: auto; width: 100% !important;}');
+    GM_addStyle('flexipage-record-home-scrollable-column {overflow-y: auto; resize: horizontal; flex: auto;}');
+    GM_addStyle('flexipage-record-home-scrollable-column.left-pinned {min-width: 300px; max-width: 50%; width: 25%;}');
+    GM_addStyle('flexipage-record-home-scrollable-column.slds-size_8-of-12 {width: 66.6666%; max-width: 97.6%; min-width:50%; padding: 12px;}');
+    GM_addStyle('flexipage-record-home-scrollable-column.slds-size_4-of-12 {max-width: 50%; width: 0px; resize: none; flex: auto;}');
+    GM_addStyle('[navex-consoleTab2_consoleTab2-host].is-pinned > section.tabContent[navex-consoleTab2_consoleTab2].active {overflow: hidden}');
 })();
