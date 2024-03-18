5314b6da137863f39e0d5f29feaed1cf788335ea
Morgan Gilroy <morgan.gilroy@gmail.com>
2024-03-18 11:09:58 +0000

    Add files via upload

---
+++ RemoveReditCrap.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Remove Reddit's promoted posts and other noise
+// @namespace    https://github.com/Delvar/TamperMonkeyScripts/
+// @version      0.5
+// @description  Get rid of Reddit's "promoted" posts and other noise
+// @author       Lyle Hanson / Morgan
+// @match        https://www.reddit.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function kill_ads() {
+        let spans = Array.prototype.slice.call( document.getElementsByTagName( "span" ) ).filter((span) => span.textContent == "promoted");
+        spans.forEach((span) => {span.parentElement.parentElement.parentElement.parentElement.style = "display: none"});
+
+        spans = document.querySelectorAll( "span.promoted-label" );
+        spans.forEach((span) => {span.parentElement.parentElement.parentElement.parentElement.style = "display: none"});
+
+        let adPostArray = Array.prototype.slice.call(document.getElementsByTagName('shreddit-ad-post'));
+        adPostArray.forEach((adPost) => {adPost.style = "display: none"});
+
+	document.getElementsByTagName("shreddit-dynamic-ad-link").forEach((span) => span.parentElement.parentElement.parentElement.parentElement.style = "display: none")
+    }
+
+    kill_ads()
+    // Hide the chat button
+    //document.getElementById("HeaderUserActions--Chat").remove();
+    // Also rerun the code each time document change (i.e new posts are added when user scroll down)
+    document.addEventListener("DOMNodeInserted", kill_ads);
+    //setInterval(kill_ads, 1000);
+})();
